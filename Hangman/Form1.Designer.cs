c5db3ef147e0e6fbbdbf9d4a498b23f0d5774a96
Hammerek <anakin165@wp.pl>
2016-05-08 00:01:28 +0200

    Initial commit
    Done

---
+++ Hangman/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.newWord = new System.Windows.Forms.Button();
+            this.checkWord = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // newWord
+            // 
+            this.newWord.Location = new System.Drawing.Point(148, 185);
+            this.newWord.Name = "newWord";
+            this.newWord.Size = new System.Drawing.Size(130, 64);
+            this.newWord.TabIndex = 0;
+            this.newWord.Text = "New";
+            this.newWord.UseVisualStyleBackColor = true;
+            this.newWord.Click += new System.EventHandler(this.newWord_Click);
+            // 
+            // checkWord
+            // 
+            this.checkWord.Location = new System.Drawing.Point(12, 185);
+            this.checkWord.Name = "checkWord";
+            this.checkWord.Size = new System.Drawing.Size(130, 64);
+            this.checkWord.TabIndex = 1;
+            this.checkWord.Text = "Try";
+            this.checkWord.UseVisualStyleBackColor = true;
+            this.checkWord.Click += new System.EventHandler(this.checkWord_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 159);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Used letters:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(84, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 261);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkWord);
+            this.Controls.Add(this.newWord);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button newWord;
+        private System.Windows.Forms.Button checkWord;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Hangman/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        string[] _words = new string[10] //array
+      {
+            "hentai", "tangentbord", "yuri", "oppai", "loli", "gothic", "programming", "witcher", "anime", "radio"
+      };
+        List<char> usedLetters = new List<char>(); //listan för bokstaver som man skrev i textboxen
+        Random _random = new Random(); //gör nytt random klass
+        char[] _currentWord; //ord som man gissar
+        char[] _currentWordShow; //ord som man visar i labeln
+        int _allTries = 0; //alla försök
+        int _missTries = 0; //missade försök
+        const int NUMBEROFTRIES = 5; //konstant variabel
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkWord.Enabled = false; //button är stängt från början
+            textBox1.KeyDown += TextBox1OnKeyDown;//med varje bokstav i textboxen använder metoden
+            textBox1.ReadOnly = true; //man får inte skriva i textboxen från början
+        }
+
+        private void TextBox1OnKeyDown(object sender, KeyEventArgs keyEventArgs)
+        {
+            if (keyEventArgs.KeyCode == Keys.Enter) //om man trycker enter då behöver man inte clicka med musen
+            {
+                checkWord_Click(sender, keyEventArgs); //använder enter som mus click
+            }
+            var a = NUMBEROFTRIES - _missTries; //tilldelar uträkning till en variabel så att man behöver inte skriva så mycket
+            if (a == 0) //gör så att man får inte MsgBox när man får inte gissa längre
+            {
+                return;
+            }
+            if (textBox1.Text.Length == a) //om antal tecken i texboxen är samma som NUMBEROFTRIES minus _misstries
+            {
+                MessageBox.Show("You can't write more letters."); //då får man inte skriva mer bokstäver
+            }
+            if (textBox1.Text.Length > a) //om antal tecken i texboxen är större än a
+            {
+                textBox1.Text = textBox1.Text.Substring(0, a); //tar bort tecken i texboxen om man försöker skriva mer än 5 
+            }
+        }
+
+        /// <summary>
+        /// Button gör så att väljer nytt ord och sätter det i labeln. 
+        /// </summary>
+        private void newWord_Click(object sender, EventArgs e)
+        {
+
+            Clear(); //Clear metod
+            var index = _random.Next(0, _words.Length - 1); //väljer en random ord från arrayen mellan 0 och words.Length
+            _currentWord = _words[index].ToCharArray(); //en random ord konverterad till Char
+            _currentWordShow = new char[_currentWord.Length]; //gör en ny char array till variabeln som man visar
+            for (var i = 0; i < _currentWord.Length; i++) //för varje tecken i currentWord
+            {
+                _currentWordShow[i] = '_';  //sätter undertecken i chararrayen
+            }
+
+            UpdateUI(); //metod för update
+        }
+
+        /// <summary>
+        /// Button som checkar om ordet innehåller tecken från textboxen. 
+        /// </summary>
+        private void checkWord_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text)) //om textbox är empty
+            {
+                return; //returnerar ingenting
+            }
+            var txt = textBox1.Text.ToLower(); //variabel till textboxen och metod som byter bokstaver till småa
+
+            textBox1.Text = ""; //tömmer textbox
+
+            foreach (var c in txt) //för varje tecken i textboxen
+            {
+                _allTries++; //ökar alla försök
+                if (usedLetters.Contains(c)) //om listan innehåller tecken från texboxen
+                {
+                    continue; //då kör man programmet vidare
+                }
+                //man lägger till bokstaven från textboxen till listan, sen konventerar man det till string för att returnera det som en liten bokstav
+                usedLetters.Add(c.ToString().ToLower().ToCharArray()[0]); //sen konverterar man det igen till CharArray
+                label4.Text = new string(usedLetters.ToArray()); //man skriver ut bokstäver till labeln
+                if (_currentWord.Contains(c)) //om ord innehåller tecken från textbox
+                {
+                    ReplaceChar(c); //man gör replacement på _currentWordShow = byter '_' för rätt tecken i textboxen
+                }
+                else
+                {
+                    _missTries++; //ökar antalet försök
+                }
+                UpdateUI(); //metod för Update
+                if (IsWin()) //om metoden är true
+                {
+                    MessageBox.Show("Du vann!");
+                    checkWord.Enabled = false;
+                    textBox1.ReadOnly = true;
+                }
+                if (_missTries >= NUMBEROFTRIES) //om man hade gissat fel 5 gånger 
+                {
+                    break; //avslutar foreach
+                }
+            }
+        }
+
+        private void Clear() //metod som tömmer variablar, textboxen, öppnar button och gör Update metoden
+        {
+            textBox1.ReadOnly = false; //man får skriva i texboxen
+            label4.Text = "";
+            usedLetters.Clear();
+            _allTries = 0; //sätter värde på en variabel som 0 
+            _missTries = 0;
+            checkWord.Enabled = true;
+            UpdateUI();
+        }
+
+        private bool IsWin() //win metod som sen visar MessageBox med meddelande i if-satsen att man gissade rätt orden 
+        {
+            return !_currentWordShow.Contains('_'); //om variabel inte innehåller undertecken då är det win
+        }
+
+        private void UpdateUI() //uppdaterar label
+        {
+            label2.Text = "Antal försök kvar: " + (NUMBEROFTRIES - _missTries); //räknar antal försök
+            if (_missTries == NUMBEROFTRIES) //om fel gissade försök är lika med fem
+            {
+                checkWord.Enabled = false; //man stänger ner knappen
+                textBox1.ReadOnly = true;
+            }
+            label1.Text = new string(_currentWordShow); //visal ord med undertecken i labeln
+        }
+
+        private void ReplaceChar(char c) //metod för att byta undertecken till tecken som finns i orden
+        {
+            for (var i = 0; i < _currentWordShow.Length; i++) //går igenom ord med undertecken
+            {
+                if (_currentWord[i] == c) //om tecken i orden är samma som i texboxen
+                {
+                    _currentWordShow[i] = c; //ändrar undertecken till rätt tecken 
+                }
+            }
+        }
+    }
+}
